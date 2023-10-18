630008b918e2288db0fc19b75c7be40a31942f71
gtmnagalla <85630305+gtmnagalla@users.noreply.github.com>
2023-10-18 16:57:16 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.21.0"
+    }
+  }
+}
+
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5BC3NYZ55WVSWHTY"
-  secret_key = "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"
 }
